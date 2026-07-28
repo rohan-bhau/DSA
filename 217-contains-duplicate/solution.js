f86ee5b9ec728541abfef1926eff8c0f15e0d968
Rohan Mia <rohanmia.org@gmail.com>
2026-07-28 19:01:28 +0600

    feat: solve Contains Duplicate (JavaScript)

---
+++ 217-contains-duplicate/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+let numbers = new Set();
+for(let i = 0; i < nums.length; i++){
+    if(numbers.has(nums[i])){
+        return true
+    }
+    numbers.add(nums[i])
+}
+return false
+
+};
