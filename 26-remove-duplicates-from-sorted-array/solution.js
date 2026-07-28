51beafa1ca0628f8706074f45ea8019602829f8b
Rohan Mia <rohanmia.org@gmail.com>
2026-07-28 18:51:01 +0600

    feat: solve Remove Duplicates from Sorted Array (JavaScript)

---
+++ 26-remove-duplicates-from-sorted-array/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 0
+    for(let i = 1; i<nums.length; i++){
+        if(nums[k]!==nums[i]){
+            k++
+            nums[k]=nums[i]
+        }
+    }
+    return k+1
+};
